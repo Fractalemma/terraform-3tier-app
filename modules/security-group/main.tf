26ac6bb1c9b80f037743a0fb1f196e6fd4419acd
Fractalemma <emmrm1324@gmail.com>
2025-10-22 17:25:21 -0600

    Fix security group reference for database access in db_sg

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -123,7 +123,7 @@ resource "aws_security_group" "db_sg" {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
-    security_groups = [aws_security_group.web_sg.id]
+    security_groups = [aws_security_group.app_sg.id]
   }
 
   egress {
